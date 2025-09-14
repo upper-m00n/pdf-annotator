217150d6a8c22d38fffd93be0bbbfe53aa43b0c0
Ashutosh Sharma <mr.proashutosh@gmail.com>
2025-09-14 09:51:15 +0530

    wrapping app.jsx

---
+++ frontend/src/pages/SignUpPage.tsx
@@ -0,0 +1,3 @@
+export default function SignupPage() {
+  return <h1 className="text-3xl font-bold">Signup Page</h1>;
+}
---
+++ frontend/src/pages/ViewerPage.tsx
@@ -0,0 +1,3 @@
+export default function ViewerPage() {
+  return <h1 className="text-3xl font-bold">PDF Viewer</h1>;
+}
